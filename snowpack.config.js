9d9a477c1db0283055e9a01fa5120fdb71d8819c
Les Orchard <me@lmorchard.com>
2021-10-09 11:59:19 -0700

    Tweak the index page to include subdirectories with index.html

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,6 +2,7 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 const path = require("path");
 const fs = require("fs");
+const glob = require("glob");
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
@@ -14,16 +15,20 @@ module.exports = {
         renderOptions: {
           async: true,
         },
+        // Behold my primitive static site generator shoehorned into snowpack
         renderData: ({ filePath }) => {
-          const sketchesDirpath = path.join(path.dirname(filePath), "sketches");
-          const sketches = fs
-            .readdirSync(sketchesDirpath)
-            .filter((subpath) =>
-              fs.statSync(path.join(sketchesDirpath, subpath)).isDirectory()
-            )
-            .map((subPath) => ({
-              name: subPath,
-              path: path.join("sketches", subPath),
+          const sketchesPath = path.join(path.dirname(filePath), "sketches");
+          const sketches = glob
+            .sync(`${sketchesPath}/**/index.html`)
+            .map((sketchPath) => ({
+              path: sketchPath.replace(`${path.dirname(filePath)}/`, ""),
+              // TODO: Maybe scrape a name & description from the HTML file
+              // TODO: Maybe use directories to come up with separate sections
+              name: sketchPath
+                .replace(`${sketchesPath}/`, "")
+                .split("/")
+                .slice(0, -1)
+                .join(" / "),
             }));
           return { sketches };
         },
