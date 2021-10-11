8d82d3c3de207049f405ac29fca59f5567abffda
lmorchard <lmorchard@users.noreply.github.com>
2021-10-11 05:44:58 +0000

    deploy: 6a0a8ec81c1a6dfee6409d9f91bc19aeac1cf091

--- sketches/2021/codesketchtober/day01/index.js
+++ sketches/2021/codesketchtober/day01/index.js
@@ -6,23 +6,13 @@ import {
   defineComponent,
   Types,
 } from "../../../../vendor/pkg/bitecs.js";
-import { hslToRgb } from "../../../../../lib/hslToRgb.js";
-import { lerp } from "../../../../../lib/transitions.js";
-import easings from "../../../../../lib/easings.js";
-import { rngIntRange } from "../../../../../lib/randoms.js";
-import * as Stats from "../../../../../lib/stats.js";
-import * as World from "../../../../../lib/world.js";
-import * as Viewport from "../../../../../lib/viewport/pixi.js";
-import {
-  Renderable,
-  RenderableShape,
-} from "../../../../../lib/viewport/index.js";
-import {
-  Position,
-  Velocity,
-  movementSystem,
-  bouncerSystem,
-} from "../../../../../lib/positionMotion.js";
+import { hslToRgb } from "../../../../lib/hslToRgb.js";
+import { lerp } from "../../../../lib/transitions.js";
+import easings from "../../../../lib/easings.js";
+import { rngIntRange } from "../../../../lib/randoms.js";
+import * as Stats from "../../../../lib/stats.js";
+import * as World from "../../../../lib/world.js";
+import * as Viewport from "../../../../lib/viewport/pixi.js";
 import { setupTwiddles } from "../../../twiddles.js";
 import { SmoothGraphics as Graphics } from "../../../../vendor/pkg/@pixi/graphics-smooth.js";
 
