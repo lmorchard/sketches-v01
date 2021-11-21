f91b56a143719fc1edbd1b12f05263ca7a5cb062
Les Orchard <me@lmorchard.com>
2021-11-20 17:07:46 -0800

    quick wasm-pack build fix

--- snowpack.config.js
+++ snowpack.config.js
@@ -15,11 +15,17 @@ module.exports = {
       },
     ],
     [
-      "snowpack-plugin-wasm-pack",
+      "@emily-curry/snowpack-plugin-wasm-pack",
       { projectPath: "./src-wasm/noise-generator" },
     ],
-    ["snowpack-plugin-wasm-pack", { projectPath: "./src-wasm/wasm_play" }],
-    ["snowpack-plugin-wasm-pack", { projectPath: "./src-wasm/perlin" }],
+    [
+      "@emily-curry/snowpack-plugin-wasm-pack",
+      { projectPath: "./src-wasm/wasm_play" },
+    ],
+    [
+      "@emily-curry/snowpack-plugin-wasm-pack",
+      { projectPath: "./src-wasm/perlin" },
+    ],
 
     // FIXME: this is breaking on an error like
     // (node:4105) UnhandledPromiseRejectionWarning: Error: Can't resolve 'sketches/2021/codesketchtober/day01/index.html' in '/home/lmorchard/devel/sketches-v01/build'
